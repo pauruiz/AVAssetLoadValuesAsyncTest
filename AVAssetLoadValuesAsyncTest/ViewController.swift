1c56e4eee904828c27e41afcc95088c80398f9d8
Pau Ruiz <pau@iosnow.net>
2017-09-22 17:35:14 +0100

    First version

--- AVAssetLoadValuesAsyncTest/ViewController.swift
+++ AVAssetLoadValuesAsyncTest/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import AVFoundation
+
+struct AVPlayerKeys {
+    static let bufferEmpty = "playbackBufferEmpty"
+    static let duration = "duration"
+    static let error = "error"
+    static let likelyToKeepUp = "playbackLikelyToKeepUp"
+    static let playable = "playable"
+    static let bufferFull = "playbackBufferFull"
+    static let rate = "rate"
+    static let status = "status"
+    static let tracks = "tracks"
+    static let timedMetadata = "timedMetadata"
+}
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    enum Urls:String {
+        case ok = "http://pau.fazerbcn.org/200.php"
+        case ko = "http://pau.fazerbcn.org/403.php"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet weak var mediaURL: UITextField!
+    @IBOutlet weak var logView: UITextView!
+    
+    @IBAction func fillWith200Page(_ sender: Any) {
+        self.mediaURL.text = Urls.ok.rawValue
     }
-
-
+    
+    @IBAction func fillWith403Page(_ sender: Any) {
+        self.mediaURL.text = Urls.ko.rawValue
+    }
+    
+    @IBAction func tryLoadValuesAsynchronouslyForMedia(_ sender: Any) {
+        guard let mediaURL = URL(string: self.mediaURL.text ?? "") else {
+            self.logView.text = "\(Date()) Invalid or empty URL, we can't continue"
+            return
+        }
+        let avAsset:AVURLAsset = AVURLAsset(url: mediaURL)
+        
+        var error : NSError?
+        
+        if avAsset.statusOfValue(forKey: AVPlayerKeys.playable, error: &error) == .loaded && avAsset.isPlayable {
+            print("Loaded and playable")
+        }
+        self.logView.text = "\(Date()) Before loadValuesAsynchronously"
+        avAsset.loadValuesAsynchronously(forKeys: [AVPlayerKeys.tracks, AVPlayerKeys.playable]) { [weak self] in
+            self?.logView.text = (self?.logView.text ?? "") + "\(Date()) We are in the callback!!!!"
+        }
+    }
 }
 
