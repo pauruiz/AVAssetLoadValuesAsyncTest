46a6201283db3d1d192df354ec5cc09f8e8fd9c8
Pau Ruiz <pau@iosnow.net>
2017-09-22 10:58:28 +0100

    Initial Commit

---
+++ AVAssetLoadValuesAsyncTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVAssetLoadValuesAsyncTest
+//
+//  Created by Ruiz, Pablo (Developer) on 22/09/2017.
+//  Copyright © 2017 BSKYB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
